5194e3c947e37c6c4e99b2e0c4172c9806996461
kringkatelyn <75917655+kringkatelyn@users.noreply.github.com>
2020-12-22 18:53:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -101,10 +101,10 @@ app.layout = html.Div([
 
 ])
 
-#@app.callback(Output('output-state', 'children'),
- #             Input('submit-button-state', 'n_clicks'),
-  #            State('input-1-state', 'value')
-   #           )
+@app.callback(Output('output-state', 'children'),
+              Input('submit-button-state', 'n_clicks'),
+              State('input-1-state', 'value')
+              )
 
 def update_output(n_clicks, input1):
     if input1 == '':
